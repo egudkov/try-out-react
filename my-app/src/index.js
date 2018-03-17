9dd26b2a406dd6c340a48d433f46896bfd44d519
Evgeny Gudkov <gudkoveyu@gmail.com>
2018-03-17 17:15:05 +0300

    Testing JSX

---
+++ my-app/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+function Article() {
+    const body = <section>body</section>;
+    return (
+        <div className="hello" style={{ color: 'red' }}>
+            <h2>title</h2>
+            {body}
+            <h3>
+                creation date: {(new Date()).toDateString()}
+            </h3>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div>
+            <h1>App Name</h1>
+            <Article />
+        </div>
+    );
+}
+
+render(<App />, document.getElementById('root'));
