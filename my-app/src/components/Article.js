8f6438fd27b812a26f13ad6e13ff830d80b4b992
Evgeny Gudkov <gudkoveyu@gmail.com>
2018-03-17 20:50:24 +0300

    Pass article content using props

--- my-app/src/components/Article.js
+++ my-app/src/components/Article.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-function Article() {
-    const body = <section>body</section>;
+function Article(props) {
+    const { article } = props
+    const body = <section>{article.text}</section>;
     return (
-        <div className="hello" style={{ color: 'red' }}>
-            <h2>title</h2>
+        <div>
+            <h2>{article.title}</h2>
             {body}
             <h3>
-                creation date: {(new Date()).toDateString()}
+                creation date: {(new Date(article.date)).toDateString()}
             </h3>
         </div>
     );
