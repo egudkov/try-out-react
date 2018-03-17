4ae96401ba6f9567aa57f8f0667ed2c0d4359175
Evgeny Gudkov <gudkoveyu@gmail.com>
2018-03-17 17:34:25 +0300

    Put components into separate files

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,26 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-
-function Article() {
-    const body = <section>body</section>;
-    return (
-        <div className="hello" style={{ color: 'red' }}>
-            <h2>title</h2>
-            {body}
-            <h3>
-                creation date: {(new Date()).toDateString()}
-            </h3>
-        </div>
-    );
-}
-
-function App() {
-    return (
-        <div>
-            <h1>App Name</h1>
-            <Article />
-        </div>
-    );
-}
+import App from './components/App'
 
 render(<App />, document.getElementById('root'));
