60f258bcf8d09545ed5f110dc2b32b20064c401c
Evgeny Gudkov <gudkoveyu@gmail.com>
2018-03-17 21:29:05 +0300

    Article state is introduced

--- my-app/src/components/Article.js
+++ my-app/src/components/Article.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Article(props) {
-    const { article } = props
-    const body = <section>{article.text}</section>;
-    return (
-        <div>
-            <h2>{article.title}</h2>
-            {body}
-            <h3>
-                creation date: {(new Date(article.date)).toDateString()}
-            </h3>
-        </div>
-    );
+class Article extends Component {
+    /*     constructor(props) {
+            super(props)
+    
+            this.state = {
+                isOpen: true
+            }
+        }
+     */
+    state = {
+        isOpen: true
+    }
+
+    render() {
+        const { article } = this.props
+        const body = this.state.isOpen && <section>{article.text}</section>;
+        return (
+            <div>
+                <h2>
+                    {article.title}
+                    <button onClick={handleClick}>close</button>
+                </h2>
+                {body}
+                <h3>
+                    creation date: {(new Date(article.date)).toDateString()}
+                </h3>
+            </div>
+        )
+    }
+}
+
+function handleClick() {
+    console.log('---', 'clicked');
+
 }
 
 export default Article
